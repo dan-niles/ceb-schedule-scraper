19676801d18b3b284c1cc1aac1a1db65decdd2db
Rafael Acosta <rafaco@gmail.com>
2022-06-03 11:55:19 -0700

    Schedules output cleanup: remove id, normalize dates, sort and print count by group

--- schedule-scraper.py
+++ schedule-scraper.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import json
 import time
 import os
@@ -50,15 +51,31 @@ data = json.loads(data["body"])
 
 def remap_data(item):
     obj = {
-        "id": item['id'],
+        #"id": item['id'],
         "group_name": item['loadShedGroupId'],
-        "starting_period": item['startTime'] + ".000Z",
-        "ending_period": item['endTime'] + ".000Z",
+        "starting_period": format_date_time(item['startTime']),
+        "ending_period": format_date_time(item['endTime']),
     }
     return obj
 
+def format_date_time(date_time):
+    # Replace middle T by a space and remove last 3 character for seconds. 
+    # Sample: from "2022-05-20T05:00:00" to "2022-05-20 05:00"
+    return date_time.replace("T", " ")[:-3]
+
+#TODO: Receive last inserted id and filter by id
+#data = filter(lambda item: item['startTime']>"2022-05-22", data)
+
+data = sorted(data, key = lambda item: (item['loadShedGroupId'], item['startTime']))
+
+# Count schedules by group
+counted = Counter((item['loadShedGroupId']) for item in data)
+output = [({'Group' : doctor}, k) for (doctor), k in counted.items()]
+print(output)
+
 
 final_data = list(map(remap_data, data))
+
 
 with open(os.path.join('output', 'schedule', 'output.json'), 'w') as outfile:
     json.dump(final_data, outfile)
