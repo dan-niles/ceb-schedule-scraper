d929eeb6726a00ff7543f9e78964b61e87d27f74
Rafael Acosta <rafaco@gmail.com>
2022-06-03 11:56:02 -0700

    Add generateSchedulesCSV.py

---
+++ generateSchedulesCSV.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+
+def read_schedules():
+    print("Reading schedules...")
+    output_schedule = "./output/schedule/output.json"
+    with open(output_schedule) as f:
+        schedule_data = json.load(f)
+    return schedule_data
+
+        
+def export_to_CSV(json_schedules):
+
+    print("Reformating schedules...")
+    data = []
+    for schedule in json_schedules:
+        row = {"group_name": schedule["group_name"], "starting_period": schedule["starting_period"], "ending_period": schedule["ending_period"]}
+        data.append(row)
+    df = pd.DataFrame(data)
+    print("Saving schedules...")
+    csvPath = "./output/schedule/output.csv"
+    df.to_csv (csvPath, index = None)
+
+
+data = read_schedules()
+export_to_CSV(data)
